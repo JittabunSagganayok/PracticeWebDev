a1dcede679cee6cb876eb6bd9b6ef0f1a346b6d0
JittabunSagganayok <123202074+JittabunSagganayok@users.noreply.github.com>
2023-01-30 21:42:15 +0700

    Add files via upload

---
+++ 1.Dad_jokes/index.js
@@ -0,0 +1,31 @@
+const btnElement = document.getElementById("btn");
+const jokeElement = document.getElementById("joke");
+const apiurl = "https://api.api-ninjas.com/v1/dadjokes?limit=2";
+
+ const apikey = "L7zf+HN3/I8Sjsv+OqNQeQ==YItTR7MTVyc0YZUk";
+
+ const options = {
+    method:"GET",
+    headers: {"X-Api-Key":apikey},
+
+ };
+
+
+
+async function getapi(){
+    try{
+        jokeElement.innerText = "Please Wait..."; 
+        btnElement.disabled=true; //ปิดไม่ให้ปุ่มกดได้
+        const response = await fetch(apiurl,options);
+        const data = await response.json();
+        console.log(data[0].joke);
+        jokeElement.innerText = data[0].joke;   
+        btnElement.disabled=true;
+    }
+    catch (error){
+        jokeElement.innerText = "Error ! Please Refresh"; 
+        console.log(error);
+    }
+}
+
+btnElement.addEventListener("click",getapi);
